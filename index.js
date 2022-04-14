97346cfb10129fc69f670398a36229c1926b44c9
Shairyar Baig <shairyarbaig@gmail.com>
2022-04-14 15:16:42 +0500

    Use sample data

--- index.js
+++ index.js
@@ -6,14 +6,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const appsignal_js_1 = __importDefault(require("./appsignal.js"));
 const fastify_1 = __importDefault(require("fastify"));
 const server = (0, fastify_1.default)();
-server.get('/ping', async (request, reply) => {
+server.get("/ping", async (request, reply) => {
     // Necessary AppSignal setup for custom instrumentation
     // https://docs.appsignal.com/nodejs/instrumentation/instrumentation.html
     const tracer = appsignal_js_1.default.tracer();
     const rootSpan = tracer.rootSpan();
     rootSpan.setName("GET /ping"); // Name of the route that will appear in AppSignal
+    const data = {
+        cars: {
+            Nissan: [
+                { model: "Sentra", doors: 4 },
+                { model: "Maxima", doors: 4 },
+                { model: "Skyline", doors: 2 },
+            ],
+            Ford: [
+                { model: "Taurus", doors: 4 },
+                { model: "Escort", doors: 4 },
+            ],
+        },
+    };
+    rootSpan.setSampleData("custom_data", { cars: data });
     rootSpan.close();
-    return 'pong\n';
+    return "pong\n";
 });
 server.listen(8080, (err, address) => {
     if (err) {
--- index.ts
+++ index.ts
@@ -1,22 +1,37 @@
-import appsignal from "./appsignal.js"
-import fastify from 'fastify'
-const server = fastify()
+import appsignal from "./appsignal.js";
+import fastify from "fastify";
+const server = fastify();
 
-server.get('/ping', async (request, reply) => {
+server.get("/ping", async (request, reply) => {
   // Necessary AppSignal setup for custom instrumentation
   // https://docs.appsignal.com/nodejs/instrumentation/instrumentation.html
   const tracer = appsignal.tracer();
   const rootSpan = tracer.rootSpan();
   rootSpan.setName("GET /ping"); // Name of the route that will appear in AppSignal
+  const data = {
+    cars: {
+      Nissan: [
+        { model: "Sentra", doors: 4 },
+        { model: "Maxima", doors: 4 },
+        { model: "Skyline", doors: 2 },
+      ],
+      Ford: [
+        { model: "Taurus", doors: 4 },
+        { model: "Escort", doors: 4 },
+      ],
+    },
+  };
+  rootSpan.setSampleData("custom_data", { cars: data });
+
   rootSpan.close();
-  
-  return 'pong\n'
-})
+
+  return "pong\n";
+});
 
 server.listen(8080, (err, address) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
-})
+  console.log(`Server listening at ${address}`);
+});
